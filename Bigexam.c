b46a8f2fe17b04c33e43a2bc8bea01c40636f8a8
Uri <uriurifallon.sc.9125268@gmail.com>
2022-04-26 14:39:07 +0700

    Finish Exam

--- Bigexam.c
+++ Bigexam.c
@@ -1,10 +1,6 @@
-//=========================== Nguyen Hoang Duong / 20212358 / DCCNTT12.10.8 ===========================
-
 #include <stdio.h>
 #include <string.h>
 
-
-// Khai báo kiểu tự định nghĩa
 struct sinhvien 
 {
     long int msv; 
@@ -12,7 +8,7 @@ struct sinhvien
     char hovaten[1000], gioitinh[1000], ngaysinh[1000], lop[1000], hocluc[1000];
 };
 
-// Nhap dữ liệu của sinh viên
+//(1)
 void nhapmang(struct sinhvien sv[], int n)
 { 
     printf(" ==NHAP THONG TIN SINH VIEN== \n"); 
@@ -28,49 +24,47 @@ void nhapmang(struct sinhvien sv[], int n)
         printf("Diem lap trinh C: "); scanf("%f",&sv[i].diemcsltc);
     }
 }
-
-// Danh sách sinh viên
-void list(struct sinhvien sv[], int n)
-{ 
+//(2)
+void dssv(struct sinhvien sv[], int n)
+{
+    printf(" ==XUAT DANH SACH SINH VIEN==");
+    printf("\n------------------------------------------------------------------------------------------------------\n");
+    printf("| %-15s| %-25s| %-11s| %-15s| %-25s|\n","Masv", "Ho ten", "Gioi tinh", "Ngay sinh", "Lop");
+    printf("------------------------------------------------------------------------------------------------------\n");
     
-    // Tính trung bình điểm sinh viên
+    for (int i = 0; i < n; i++)
+    {
+        printf("| %-15ld| %-25s| %-11s| %-15s| %-25s|\n",
+        sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].lop);
+    }
+
+    printf("------------------------------------------------------------------------------------------------------\n");
+}
+
+
+//(3)
+void diemtb(struct sinhvien sv[], int n)
+{
     for(int i=0; i < n; i++)
     {
         sv[i].diemtb = (sv[i].diemtcc + sv[i].diemcsltc + sv[i].diemth)/3;
     }
 
-    // Khai báo học lực
-    char gioi[20] = "Gioi";
-    char kha[20]  = "Kha";
-    char tb[20]   = "Trung binh";
-    char yeu[20]  = "Yeu";
-    
-    // Xếp loại học lực
-    for(int i=0; i < n; i++)
-    {
-        if(sv[i].diemtb >= 8) strcpy(sv[i].hocluc,gioi);
-        if(sv[i].diemtb >= 6.5 && sv[i].diemtb < 8) strcpy(sv[i].hocluc,kha);
-        if(sv[i].diemtb >= 5 && sv[i].diemtb < 6.5) strcpy(sv[i].hocluc,tb);
-        if(sv[i].diemtb <5) strcpy(sv[i].hocluc,yeu);
-    }
-    
-    // Xuất tiêu đề bảng
     printf(" ==XUAT DANH SACH SINH VIEN==");
-    printf("\n---------------------------------------------------------------------------------------------------------------\n");
-    printf("| %-15s| %-25s| %-11s| %-15s| %-17s| %-15s|\n","Masv", "Ho ten", "Gioi tinh", "Ngay sinh","Diem trung binh", "Xep loai");
-    printf("---------------------------------------------------------------------------------------------------------------\n");
+    printf("\n-------------------------------------------------------------------------------------------------------------------------\n");
+    printf("| %-15s| %-25s| %-11s| %-15s| %-25s| %-17s|\n","Masv", "Ho ten", "Gioi tinh", "Ngay sinh","Lop", "Diem trung binh");
+    printf("-------------------------------------------------------------------------------------------------------------------------\n");
     
-    // Xuất nội dung bảng
     for (int i = 0; i < n; i++)
     {
-        printf("| %-15ld| %-25s| %-11s| %-15s| %-17.2f| %-15s|\n",
-        sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].diemtb,sv[i].hocluc);
+        printf("| %-15ld| %-25s| %-11s| %-15s| %-25s| %-17.2f|\n",
+        sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].lop,sv[i].diemtb);
     }
 
-    printf("---------------------------------------------------------------------------------------------------------------\n");
+    printf("-------------------------------------------------------------------------------------------------------------------------\n");
 }
 
-// Sắp xếp sinh vien theo điểm trung bình 
+//(4)
 void sapxep(struct sinhvien sv[], int n)
 {
     for(int i=0; i < n-1; i++)
@@ -85,69 +79,85 @@ void sapxep(struct sinhvien sv[], int n)
             }
         }
     }
-    
-    // Xuất tiêu đề bảng
+
     printf(" ==XUAT DANH SACH SINH VIEN==");
-    printf("\n---------------------------------------------------------------------------------------------------------------\n");
-    printf("| %-15s| %-25s| %-11s| %-15s| %-17s| %-15s|\n","Masv", "Ho ten", "Gioi tinh", "Ngay sinh","Diem trung binh", "Xep loai");
-    printf("---------------------------------------------------------------------------------------------------------------\n");
+    printf("\n-------------------------------------------------------------------------------------------------------------------------\n");
+    printf("| %-15s| %-25s| %-11s| %-15s| %-25s| %-17s|\n","Masv", "Ho ten", "Gioi tinh", "Ngay sinh", "Lop", "Diem trung binh");
+    printf("-------------------------------------------------------------------------------------------------------------------------\n");
     
-    // Xuất nội dung bảng đã sắp xếp
     for (int i = 0; i < n; i++)
     {
-        printf("| %-15ld| %-25s| %-11s| %-15s| %-17.2f| %-15s|\n",
-        sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].diemtb,sv[i].hocluc);
+        printf("| %-15ld| %-25s| %-11s| %-15s| %-25s| %-17.2f|\n",
+        sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].lop,sv[i].diemtb);
     }
 
-    printf("---------------------------------------------------------------------------------------------------------------\n");
-
+    printf("-------------------------------------------------------------------------------------------------------------------------\n");
 }
 
-// Nhap file
+//(5)
+void hocluc(struct sinhvien sv[], int n)
+{ 
+    char gioi[20] = "Gioi";
+    char kha[20]  = "Kha";
+    char tb[20]   = "Trung binh";
+    char yeu[20]  = "Yeu";
+    
+    for(int i=0; i < n; i++)
+    {
+        if(sv[i].diemtb >= 8) strcpy(sv[i].hocluc,gioi);
+        if(sv[i].diemtb >= 6.5 && sv[i].diemtb < 8) strcpy(sv[i].hocluc,kha);
+        if(sv[i].diemtb >= 5 && sv[i].diemtb < 6.5) strcpy(sv[i].hocluc,tb);
+        if(sv[i].diemtb <5) strcpy(sv[i].hocluc,yeu);
+    }
+
+    printf(" ==XUAT DANH SACH SINH VIEN==");
+    printf("\n------------------------------------------------------------------------------------------------------------------------------------------\n");
+    printf("| %-15s| %-25s| %-11s| %-15s| %-25s| %-17s| %-15s|\n","Masv", "Ho ten", "Gioi tinh", "Ngay sinh", "Lop", "Diem trung binh", "Xep loai");
+    printf("------------------------------------------------------------------------------------------------------------------------------------------\n");
+    
+    for (int i = 0; i < n; i++)
+    {
+        printf("| %-15ld| %-25s| %-11s| %-15s| %-25s| %-17.2f| %-15s|\n",
+        sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].lop,sv[i].diemtb,sv[i].hocluc);
+    }
+
+    printf("------------------------------------------------------------------------------------------------------------------------------------------\n");
+}
+
+//(6)
 void nhapfile(struct sinhvien sv[], int n)
 {
     printf("Noi dung da duoc nhap vao: a-test.txt\n");
-
-    // Khai báo con trỏ file + Mở file
-    FILE * fptxt = fopen("a-test.txt=","w+"); 
+    FILE * fptxt = fopen("a-test.txt","w+"); 
     
-    // Ghi file 
     for(int i=0; i<n; i++)
     {
-        fprintf(fptxt ,"| %-15ld| %-25s| %-11s| %-15s| %-17.2f| %-15s|\n",
-        sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].diemtb,sv[i].hocluc);
+        fprintf(fptxt ,"%-15ld %-25s %-11s %-15s %-25s %-17.2f %-15s\n",
+        sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].lop,sv[i].diemtb,sv[i].hocluc);
     }
- 
-    // Đóng file
+
     fclose(fptxt);
 }
 
-// xuat file
 void xuatfile(struct sinhvien sv[], int n)
 {
+    FILE * fptxt = fopen("a-test.txt","r"); 
 
-    // Khai báo con trỏ file + Mở file
-    FILE * fptxt = fopen("a-test.txt","w+"); 
-
-    // Xuất file
     for(int i=0; i<n; i++)
     {
-        fscanf(fptxt ," %15d %25s %11s %15s %17f %15s",
-        &sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,&sv[i].diemtb,sv[i].hocluc);
+        fscanf(fptxt ," %15d %25s %11s %15s %25s %17f %15s",
+        &sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].lop,&sv[i].diemtb,sv[i].hocluc);
+        printf("%15d %25s %11s %15s %25s %17.2f %15s\n",sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].lop,sv[i].diemtb,sv[i].hocluc);
     }
-    for(int i=0; i < n; i++)
-    {
-        printf("| %-15ld| %-25s| %-11s| %-15s| %-17.2f| %-15s|\n",
-        sv[i].msv,sv[i].hovaten,sv[i].gioitinh,sv[i].ngaysinh,sv[i].diemtb,sv[i].hocluc);
-    }
+    
 
-    // Đóng file
     fclose(fptxt);
 }
 
 int main()
 {
-    int n; printf("Nhap so luong sinh vien : "); scanf("%d",&n); struct sinhvien sv[n];
+    int n; printf("Nhap so luong sinh vien : "); scanf("%d",&n);
+    struct sinhvien sv[n];
     int chon; 
     
     do
@@ -168,8 +178,10 @@ int main()
         switch(chon) 
         {
             case 1: nhapmang(sv,n); break;
-            case 2: list(sv,n); break; 
-            case 4: sapxep(sv,n); break;
+            case 2: dssv(sv,n);break;
+            case 3: diemtb(sv,n);break;
+            case 4: sapxep(sv,n);break;
+            case 5: hocluc(sv,n);break;
             case 6: nhapfile(sv,n); xuatfile(sv,n); break;
         }
     } while (chon !=0 );
